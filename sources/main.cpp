67cdb5a51fbcd9b5cb79721863a9bf242fdd5433
Brian Chau <brian.chau@outlook.com>
2021-04-17 14:15:47 -0700

    Initial commit

---
+++ sources/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <curl/curl.h>
+ 
+int main( void )
+{
+    CURL *curl;
+    CURLcode res;
+ 
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    
+    return 0;
+}
